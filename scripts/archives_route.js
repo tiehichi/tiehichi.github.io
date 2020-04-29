e8b1a360721b83a6f46152010a9cecf3b85dbaa4
Dai Bingzhi <daibingzhi9519@outlook.com>
2020-04-29 15:21:03 +0800

    update

--- scripts/archives_route.js
+++
@@ -1,7 +0,0 @@
-hexo.extend.generator.register('archive', function(locals){
-    return {
-      path: 'archives/index.html',
-      data: locals.posts,
-      layout: ['index', 'years']
-    }
-  });
